d6942df76e8b3b90ff928501e8deff0e71a310bf
John obiko <obi22004@byui.edu>
2024-05-23 22:37:17 +0300

    ghkkhg

--- js/main.js
+++ js/main.js
@@ -321,9 +321,6 @@ function updateFavicon() {
   const matcher = window.matchMedia('(prefers-color-scheme: dark)');
   matcher.addListener(updateFavicon);
 
-  window.onload = function() {
-	// Once the page is fully loaded, fade out the preloader
-	document.querySelector('.preloader').style.opacity = '0';
-};
+ 
   // Initial update
   updateFavicon();
