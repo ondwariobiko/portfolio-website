eca6ab1628b2cafdfcf36363e3757fb79abd98a9
John obiko <obi22004@byui.edu>
2025-04-22 22:22:40 +0300

    js changes

--- js/main.js
+++ js/main.js
@@ -1,285 +1,228 @@
 ;(function () {
-	
-	'use strict';
-
-	var isMobile = {
-		Android: function() {
-			return navigator.userAgent.match(/Android/i);
-		},
-			BlackBerry: function() {
-			return navigator.userAgent.match(/BlackBerry/i);
-		},
-			iOS: function() {
-			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-		},
-			Opera: function() {
-			return navigator.userAgent.match(/Opera Mini/i);
-		},
-			Windows: function() {
-			return navigator.userAgent.match(/IEMobile/i);
-		},
-			any: function() {
-			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-		}
-	};
-
-	var mobileMenuOutsideClick = function() {
-
-		$(document).click(function (e) {
-	    var container = $("#fh5co-offcanvas, .js-fh5co-nav-toggle");
-	    if (!container.is(e.target) && container.has(e.target).length === 0) {
-
-	    	if ( $('body').hasClass('offcanvas') ) {
-
-    			$('body').removeClass('offcanvas');
-    			$('.js-fh5co-nav-toggle').removeClass('active');
-				
-	    	}
-	    
-	    	
-	    }
-		});
-
-	};
-
-
-	var offcanvasMenu = function() {
-
-		$('#page').prepend('<div id="fh5co-offcanvas" />');
-		$('#page').prepend('<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle fh5co-nav-white"><i></i></a>');
-		var clone1 = $('.menu-1 > ul').clone();
-		$('#fh5co-offcanvas').append(clone1);
-		var clone2 = $('.menu-2 > ul').clone();
-		$('#fh5co-offcanvas').append(clone2);
-
-		$('#fh5co-offcanvas .has-dropdown').addClass('offcanvas-has-dropdown');
-		$('#fh5co-offcanvas')
-			.find('li')
-			.removeClass('has-dropdown');
-
-		// Hover dropdown menu on mobile
-		$('.offcanvas-has-dropdown').mouseenter(function(){
-			var $this = $(this);
-
-			$this
-				.addClass('active')
-				.find('ul')
-				.slideDown(500, 'easeOutExpo');				
-		}).mouseleave(function(){
-
-			var $this = $(this);
-			$this
-				.removeClass('active')
-				.find('ul')
-				.slideUp(500, 'easeOutExpo');				
-		});
-
-
-		$(window).resize(function(){
-
-			if ( $('body').hasClass('offcanvas') ) {
-
-    			$('body').removeClass('offcanvas');
-    			$('.js-fh5co-nav-toggle').removeClass('active');
-				
-	    	}
-		});
-	};
-
-
-	var burgerMenu = function() {
-
-		$('body').on('click', '.js-fh5co-nav-toggle', function(event){
-			var $this = $(this);
-
-
-			if ( $('body').hasClass('overflow offcanvas') ) {
-				$('body').removeClass('overflow offcanvas');
-			} else {
-				$('body').addClass('overflow offcanvas');
-			}
-			$this.toggleClass('active');
-			event.preventDefault();
-
-		});
-	};
-	
-
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
-				
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .animate-box.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn animated-fast');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft animated-fast');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight animated-fast');
-							} else {
-								el.addClass('fadeInUp animated-fast');
-							}
-
-							el.removeClass('item-animate');
-						},  k * 200, 'easeInOutExpo' );
-					});
-					
-				}, 100);
-				
-			}
-
-		} , { offset: '85%' } );
-	};
-
-
-	var dropdown = function() {
-
-		$('.has-dropdown').mouseenter(function(){
-
-			var $this = $(this);
-			$this
-				.find('.dropdown')
-				.css('display', 'block')
-				.addClass('animated-fast fadeInUpMenu');
-
-		}).mouseleave(function(){
-			var $this = $(this);
-
-			$this
-				.find('.dropdown')
-				.css('display', 'none')
-				.removeClass('animated-fast fadeInUpMenu');
-		});
-
-	};
-
-
-	var goToTop = function() {
-
-		$('.js-gotop').on('click', function(event){
-			
-			event.preventDefault();
-
-			$('html, body').animate({
-				scrollTop: $('html').offset().top
-			}, 500, 'easeInOutExpo');
-			
-			return false;
-		});
-
-		$(window).scroll(function(){
-
-			var $win = $(window);
-			if ($win.scrollTop() > 200) {
-				$('.js-top').addClass('active');
-			} else {
-				$('.js-top').removeClass('active');
-			}
-
-		});
-	
-	};
-
-
-	// Loading page
-	var loaderPage = function() {
-		$(".fh5co-loader").fadeOut("slow");
-	};
-
-	var counter = function() {
-		$('.js-counter').countTo({
-			 formatter: function (value, options) {
-	      return value.toFixed(options.decimals);
-	    },
-		});
-	};
-
-
-	var sliderMain = function() {
-		
-	  	$('#fh5co-hero .flexslider').flexslider({
-			animation: "fade",
-			slideshowSpeed: 5000,
-			directionNav: true,
-			start: function(){
-				setTimeout(function(){
-					$('.slider-text').removeClass('animated fadeInUp');
-					$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
-				}, 500);
-			},
-			before: function(){
-				setTimeout(function(){
-					$('.slider-text').removeClass('animated fadeInUp');
-					$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
-				}, 500);
-			}
-
-	  	});
-
-	};
-
-	var stickyFunction = function() {
-
-		var h = $('.image-content').outerHeight();
-
-		if ($(window).width() <= 992 ) {
-			$("#sticky_item").trigger("sticky_kit:detach");
-		} else {
-			$('.sticky-parent').removeClass('stick-detach');
-			$("#sticky_item").trigger("sticky_kit:detach");
-			$("#sticky_item").trigger("sticky_kit:unstick");
-		}
-
-		$(window).resize(function(){
-			var h = $('.image-content').outerHeight();
-			$('.sticky-parent').css('height', h);
-
-
-			if ($(window).width() <= 992 ) {
-				$("#sticky_item").trigger("sticky_kit:detach");
-			} else {
-				$('.sticky-parent').removeClass('stick-detach');
-				$("#sticky_item").trigger("sticky_kit:detach");
-				$("#sticky_item").trigger("sticky_kit:unstick");
-
-				$("#sticky_item").stick_in_parent();
-			}
-			
-
-			
-
-		});
-
-		$('.sticky-parent').css('height', h);
-
-		$("#sticky_item").stick_in_parent();
-
-	};
-
-	
-	$(function(){
-		mobileMenuOutsideClick();
-		offcanvasMenu();
-		burgerMenu();
-		contentWayPoint();
-		sliderMain();
-		dropdown();
-		goToTop();
-		loaderPage();
-		stickyFunction();
-	});
-
-	$(document).ready(function(){
+    'use strict';
+
+    // ==================== IMPROVEMENTS ====================
+    // 1. Better mobile detection using modern API first
+    const isMobile = {
+        any: function() {
+            return ( 
+                navigator.maxTouchPoints > 0 ||
+                navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i)
+            );
+        }
+    };
+
+    // 2. Debounced resize handler
+    const debounce = function(func, wait) {
+        let timeout;
+        return function() {
+            const context = this, args = arguments;
+            clearTimeout(timeout);
+            timeout = setTimeout(function() {
+                func.apply(context, args);
+            }, wait);
+        };
+    };
+
+    // 3. Throttled scroll handler
+    const throttle = function(func, limit) {
+        let lastFunc;
+        let lastRan;
+        return function() {
+            const context = this;
+            const args = arguments;
+            if (!lastRan) {
+                func.apply(context, args);
+                lastRan = Date.now();
+            } else {
+                clearTimeout(lastFunc);
+                lastFunc = setTimeout(function() {
+                    if ((Date.now() - lastRan) >= limit) {
+                        func.apply(context, args);
+                        lastRan = Date.now();
+                    }
+                }, limit - (Date.now() - lastRan));
+            }
+        };
+    };
+    // ==================== END IMPROVEMENTS ====================
+
+    var mobileMenuOutsideClick = function() {
+        $(document).click(function (e) {
+            var container = $("#fh5co-offcanvas, .js-fh5co-nav-toggle");
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
+                if ( $('body').hasClass('offcanvas') ) {
+                    $('body').removeClass('offcanvas');
+                    $('.js-fh5co-nav-toggle').removeClass('active');
+                }
+            }
+        });
+    };
+
+    var offcanvasMenu = function() {
+        $('#page').prepend('<div id="fh5co-offcanvas" />');
+        $('#page').prepend('<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle fh5co-nav-white"><i></i></a>');
+        
+        // Clone menus once
+        $('#fh5co-offcanvas')
+            .append($('.menu-1 > ul').clone())
+            .append($('.menu-2 > ul').clone())
+            .find('.has-dropdown')
+            .addClass('offcanvas-has-dropdown')
+            .removeClass('has-dropdown');
+
+        // Optimized hover handling
+        $('.offcanvas-has-dropdown').hover(
+            function(){ // mouseenter
+                $(this)
+                    .addClass('active')
+                    .find('ul')
+                    .stop(true, true)
+                    .slideDown(500, 'easeOutExpo');
+            }, 
+            function(){ // mouseleave
+                $(this)
+                    .removeClass('active')
+                    .find('ul')
+                    .stop(true, true)
+                    .slideUp(500, 'easeOutExpo');
+            }
+        );
+
+        // Debounced resize
+        $(window).on('resize', debounce(function(){
+            if ( $('body').hasClass('offcanvas') ) {
+                $('body').removeClass('offcanvas');
+                $('.js-fh5co-nav-toggle').removeClass('active');
+            }
+        }, 200));
+    };
+
+    var burgerMenu = function() {
+        $('body').on('click', '.js-fh5co-nav-toggle', function(event){
+            event.preventDefault();
+            $(this).toggleClass('active');
+            $('body').toggleClass('overflow offcanvas');
+        });
+    };
+
+    var contentWayPoint = function() {
+        var i = 0;
+        $('.animate-box').waypoint(function(direction) {
+            if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
+                i++;
+                $(this.element).addClass('item-animate');
+                
+                setTimeout(function(){
+                    $('body .animate-box.item-animate').each(function(k){
+                        var el = $(this);
+                        setTimeout(function() {
+                            var effect = el.data('animate-effect') || 'fadeInUp';
+                            el.addClass(effect + ' animated-fast')
+                              .removeClass('item-animate');
+                        }, k * 200);
+                    });
+                }, 100);
+            }
+        }, { offset: '85%' });
+    };
+
+    var dropdown = function() {
+        $('.has-dropdown').hover(
+            function(){ // mouseenter
+                $(this).find('.dropdown')
+                    .css('display', 'block')
+                    .addClass('animated-fast fadeInUpMenu');
+            },
+            function(){ // mouseleave
+                $(this).find('.dropdown')
+                    .css('display', 'none')
+                    .removeClass('animated-fast fadeInUpMenu');
+            }
+        );
+    };
+
+    var goToTop = function() {
+        $('.js-gotop').on('click', function(event){
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: 0
+            }, 500, 'easeInOutExpo');
+        });
+
+        // Throttled scroll handler
+        $(window).on('scroll', throttle(function(){
+            $('.js-top').toggleClass('active', $(window).scrollTop() > 200);
+        }, 100));
+    };
+
+    var loaderPage = function() {
+        $(".fh5co-loader").fadeOut("slow");
+    };
+
+    var counter = function() {
+        $('.js-counter').countTo({
+            formatter: function (value, options) {
+                return value.toFixed(options.decimals);
+            },
+        });
+    };
+
+    var sliderMain = function() {
+        $('#fh5co-hero .flexslider').flexslider({
+            animation: "fade",
+            slideshowSpeed: 5000,
+            directionNav: true,
+            start: function(){
+                setTimeout(function(){
+                    $('.slider-text').removeClass('animated fadeInUp');
+                    $('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
+                }, 500);
+            },
+            before: function(){
+                setTimeout(function(){
+                    $('.slider-text').removeClass('animated fadeInUp');
+                    $('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
+                }, 500);
+            }
+        });
+    };
+
+    var stickyFunction = function() {
+        var updateSticky = function() {
+            var h = $('.image-content').outerHeight();
+            $('.sticky-parent').css('height', h);
+
+            if ($(window).width() <= 992 ) {
+                $("#sticky_item").trigger("sticky_kit:detach");
+            } else {
+                $('.sticky-parent').removeClass('stick-detach');
+                $("#sticky_item").trigger("sticky_kit:detach");
+                $("#sticky_item").trigger("sticky_kit:unstick");
+                $("#sticky_item").stick_in_parent();
+            }
+        };
+
+        // Initial setup
+        updateSticky();
+        
+        // Debounced resize
+        $(window).on('resize', debounce(updateSticky, 200));
+    };
+
+    // Initialize everything
+    $(function(){
+        mobileMenuOutsideClick();
+        offcanvasMenu();
+        burgerMenu();
+        contentWayPoint();
+        sliderMain();
+        dropdown();
+        goToTop();
+        loaderPage();
+        stickyFunction();
+        
+        // Testimonial slider
         $('.testimonial-slider').slick({
             infinite: true,
             slidesToShow: 1,
@@ -292,58 +235,34 @@
             nextArrow: '<button type="button" class="slick-next">Next</button>'
         });
     });
-	// Get all <a> tags on the page
-const links = document.querySelectorAll('a');
 
-// Loop through each link
-links.forEach(link => {
-  // Check if the link is an external link
-  if (link.hostname !== window.location.hostname) {
-    // Add the target="_blank" attribute
-    link.setAttribute('target', '_blank');
-  }
-});
-}());
-
-// Function to update favicon based on color scheme
-function updateFavicon() {
-    const lightSchemeIcon = document.querySelector('link#light-scheme-icon');
-    const darkSchemeIcon = document.querySelector('link#dark-scheme-icon');
-    const matcher = window.matchMedia('(prefers-color-scheme: dark)');
-
-    // Add or remove the appropriate favicon based on the match
-    if (matcher.matches) {
-      document.head.append(darkSchemeIcon);
-      lightSchemeIcon.remove();
-    } else {
-      document.head.append(lightSchemeIcon);
-      darkSchemeIcon.remove();
-    }
-  }
-
-
-  window.onload = function() {
-    document.querySelectorAll('.image-item img').forEach(function(img) {
-        img.oncontextmenu = function(e) {
-            e.preventDefault();
-        };
+    // External links handler
+    document.querySelectorAll('a').forEach(link => {
+        if (link.hostname !== window.location.hostname) {
+            link.setAttribute('target', '_blank');
+            link.setAttribute('rel', 'noopener noreferrer'); // Security improvement
+        }
     });
-};
-  // Set up a listener for changes in color scheme
-  const matcher = window.matchMedia('(prefers-color-scheme: dark)');
-  matcher.addListener(updateFavicon);
 
- 
-  // Initial update
-  updateFavicon();
+    // Right-click protection
+    document.querySelectorAll('.image-item img').forEach(img => {
+        img.addEventListener('contextmenu', e => e.preventDefault());
+    });
 
-// Sticky Navbar
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 100) { // Adjust this value based on when you want the sticky effect to trigger
-        $('.fh5co-nav').addClass('sticky');
-        $('body').addClass('sticky-nav');
-    } else {
-        $('.fh5co-nav').removeClass('sticky');
-        $('body').removeClass('sticky-nav');
+    // Favicon switcher
+    function updateFavicon() {
+        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        document.head.append(
+            document.querySelector(prefersDark ? 'link#dark-scheme-icon' : 'link#light-scheme-icon')
+        );
     }
-}); 
+    window.matchMedia('(prefers-color-scheme: dark)').addListener(updateFavicon);
+    updateFavicon();
+
+    // Sticky navbar with throttling
+    $(window).on('scroll', throttle(function() {
+        const scrollTop = $(this).scrollTop();
+        $('.fh5co-nav').toggleClass('sticky', scrollTop > 100);
+        $('body').toggleClass('sticky-nav', scrollTop > 100);
+    }, 100));
+}());
